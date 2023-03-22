27af6fc83f026a4c6b63f82345f9b3b0e5a27a32
Giovani Meza <giovani.meza@claro.com.gt>
2023-03-22 15:22:25 -0600

    Prueba docker

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: [ './navbar.component.css' ]
+})
+export class NavbarComponent {
+  version = environment.version;
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+}
